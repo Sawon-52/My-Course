201ed6650d4b6349cb8085df25e59e665c074ea6
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-03-29 16:41:51 +0600

    Showing Credit Remaining all time positive

--- src/Component/Craditremaining/Craditremaining.jsx
+++ src/Component/Craditremaining/Craditremaining.jsx
@@ -1,12 +1,9 @@
 import PropTypes from "prop-types";
 
 const Craditremaining = ({ credit = 0 }) => {
-  
-  
   return (
-    
     <div className="my-5">
-      <h2 className=" text-2xl font-bold text-blue-500 ">Credit Hour Remaining {credit} hr</h2>
+      <h2 className=" text-2xl font-bold text-blue-500 ">Credit Hour Remaining {Math.abs(credit - 20)} hr</h2>
     </div>
   );
 };
